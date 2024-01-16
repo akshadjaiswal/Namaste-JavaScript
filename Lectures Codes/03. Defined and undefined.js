6a15b4fd8da34ea74767142e7a2c4136f48e3961
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-16 19:39:17 +0530

    check

--- Lectures Codes/03. Defined and undefined.js
+++ Lectures Codes/03. Defined and undefined.js
@@ -2,3 +2,15 @@ console.log(x); // undefined
 var x = 25;
 console.log(x); // 25
 console.log(a); // Uncaught ReferenceError: a is not defined
+
+//checking defined and undefined cases
+console.log(a);
+var a;
+console.log(a);
+a = 5;
+if (a === undefined) {
+    console.log("a is undefined")
+}
+else {
+    console.log("a is not undefined")
+}
