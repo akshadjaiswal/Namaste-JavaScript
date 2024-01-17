1c0de625b0068f68599a9afc2452d97a1eff08d7
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-17 21:27:16 +0530

    text formatting

--- Lectures Codes/06 Block Scope and Shadowing in Javascript.js
+++ Lectures Codes/06 Block Scope and Shadowing in Javascript.js
@@ -1,15 +1,15 @@
 //Block
-if(true){
+if (true) {
     //compound statement
-    var x=100;
+    var x = 100;
     console.log(x);
 }
 
 //Block Scope
 {
-    var a=10;
-    let b=20;
-    const c=30;
+    var a = 10;
+    let b = 20;
+    const c = 30;
     console.log(a);
     console.log(b);
     console.log(c);
@@ -19,13 +19,13 @@ console.log(a);
 // console.log(c);
 
 //Shadowing
-var x=100;//This variable is shadowed by the x varioable present in the block
-let y=200;
-const z=300;
+var x = 100;//This variable is shadowed by the x varioable present in the block
+let y = 200;
+const z = 300;
 {
-    var x=10;//It shadows the variable x outside the block
-    let y=20;
-    const z=30;
+    var x = 10;//It shadows the variable x outside the block
+    let y = 20;
+    const z = 30;
     console.log(x);
     console.log(y);
     console.log(z);
@@ -35,11 +35,11 @@ console.log(y);//It will print the y variable present outside the block
 console.log(z);//It will print the z variable present outside the block
 
 //Shadowing for function
-var o=10;
-function q(){
-    var o=25;
+var o = 10;
+function q() {
+    var o = 25;
     console.log(window.o);//Inside block scope o variable 
-    window.o=20;
+    window.o = 20;
 }
 q();
 console.log(o);//Outside block scope o variable 
