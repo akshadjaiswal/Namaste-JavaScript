a89dd7c8864c29ba5414a4ec370f799742010ce4
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-17 21:15:23 +0530

    Shadowing

--- Lectures Codes/06 Block Scope and Shadowing in Javascript.js
+++ Lectures Codes/06 Block Scope and Shadowing in Javascript.js
@@ -17,3 +17,19 @@ if(true){
 console.log(a);
 // console.log(b); //Uncaught reference error due to the block scope
 // console.log(c);
+
+//Shadowing
+var x=100;//This variable is shadowed by the x varioable present in the block
+let y=200;
+const z=300;
+{
+    var x=10;//It shadows the variable x outside the block
+    let y=20;
+    const z=30;
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+console.log(x);
+console.log(y);//It will print the y variable present outside the block
+console.log(z);//It will print the z variable present outside the block
