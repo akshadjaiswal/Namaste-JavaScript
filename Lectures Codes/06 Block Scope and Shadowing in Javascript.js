de50bfc806f5363d9fe1900f788f0a5261e395cd
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-17 21:14:05 +0530

    Block Scope

--- Lectures Codes/06 Block Scope and Shadowing in Javascript.js
+++ Lectures Codes/06 Block Scope and Shadowing in Javascript.js
@@ -4,3 +4,16 @@ if(true){
     var x=100;
     console.log(x);
 }
+
+//Block Scope
+{
+    var a=10;
+    let b=20;
+    const c=30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a);
+// console.log(b); //Uncaught reference error due to the block scope
+// console.log(c);
