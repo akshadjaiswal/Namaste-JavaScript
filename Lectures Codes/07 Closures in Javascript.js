f3274346aff8ed6b78363e257051376002333632
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-17 23:22:21 +0530

    Corner cases in closures

--- Lectures Codes/07 Closures in Javascript.js
+++ Lectures Codes/07 Closures in Javascript.js
@@ -10,3 +10,17 @@ function x() {
 var z = x();
 console.log(z);//It will return what is present in the function y.
 z();//It will still return the value of a.
+
+//Corner Cases
+function b() {
+    var e = 99;
+    function w() {
+        var c = 9;
+        function d() {
+            console.log(c, e);
+        }
+        d();
+    }
+    w();
+}
+b();
