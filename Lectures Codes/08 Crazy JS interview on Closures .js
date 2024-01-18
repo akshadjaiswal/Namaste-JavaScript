4131e075a64dc71b5010f3ac4383f0214d5fcf71
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-18 19:44:22 +0530

    Changing position of variable

--- Lectures Codes/08 Crazy JS interview on Closures .js
+++ Lectures Codes/08 Crazy JS interview on Closures .js
@@ -1,9 +1,20 @@
 //Example on closure
-function outer(){
-    var a=10;
-    function inner(){
+function outer() {
+    var a = 10;
+    function inner() {
         console.log(a);
     }
     return inner;
 }
 outer()();//It still remembers the value of a variable.
+
+//Changing postion of variable
+function outer() {
+    // var a=10;
+    function inner() {
+        console.log(a);
+    }
+    var a = 10;//Even if we more the position of variable declartion it is still closure
+    return inner;
+}
+outer()();
