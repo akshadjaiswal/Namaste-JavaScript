e8ae2abdd9c3ec572ff298723ec26f5baf69fe4b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-18 23:53:46 +0530

    tricky question

--- Lectures Codes/08 Set-Timeout + Closures interview questions.js
+++ Lectures Codes/08 Set-Timeout + Closures interview questions.js
@@ -7,3 +7,14 @@ function x() {
     console.log("Hello from Akshad");//It will first print this console then wait for 3000ms to print the value of i.//JavaScript waits for no one.
 }
 x();//This set timeout will print the value of i=1 after 3 sec.
+
+//Print the number 1 to 5 after each seconds using setTimeout
+function x() {
+    //let has block scope and it creates the new copies i after every loop
+    for (let i = 1; i <= 5; i++) {
+        setTimeout(function () {
+            console.log(i);
+        }, i * 1000);
+    }
+}
+x();
