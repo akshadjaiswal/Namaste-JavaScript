da31f43b83586d884726ac54da71fc9bd0397bb1
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-19 00:04:58 +0530

    By using var and closure

--- Lectures Codes/08 Set-Timeout + Closures interview questions.js
+++ Lectures Codes/08 Set-Timeout + Closures interview questions.js
@@ -18,3 +18,18 @@ function x() {
     }
 }
 x();
+
+//Print the number 1 to 5 after each seconds using setTimeout by using var
+function x() {
+    //let has block scope and it creates the new copies i after every loop
+    for (var i = 1; i <= 5; i++) {
+        function close(x) {
+            setTimeout(function () {
+                console.log(x);
+            }, x * 1000);
+        }
+        close(i);//every time we call this function again it will provide the new value of i and now it will work with var
+    }
+    console.log("Hello from Akshad ")
+}
+x();
