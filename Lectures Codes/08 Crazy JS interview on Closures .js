3ba5c643919975ab0f4c7030ecb8b9b398d59aad
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-18 20:30:01 +0530

    Function Constructor in JavaScript

--- Lectures Codes/08 Crazy JS interview on Closures .js
+++ Lectures Codes/08 Crazy JS interview on Closures .js
@@ -86,3 +86,23 @@ counter1();
 counter1();
 counter1();
 counter1();
+
+//Function Constructor in javascript using closures
+function Counter() {
+    count = 0;
+    this.incrementCounter = function () {
+        count++;
+        console.log(count);
+    }
+    this.decrementCounter = function () {
+        count--;
+        console.log(count);
+    }
+}
+var counter1 = new Counter();
+counter1.incrementCounter()
+counter1.incrementCounter()
+counter1.decrementCounter()
+counter1.decrementCounter()
+counter1.incrementCounter()
+
