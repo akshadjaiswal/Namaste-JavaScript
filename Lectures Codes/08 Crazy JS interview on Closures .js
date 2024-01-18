db143ea483e5413d154487c551ae1966a1dc05b3
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-18 20:00:14 +0530

    Conflicting name pf variable

--- Lectures Codes/08 Crazy JS interview on Closures .js
+++ Lectures Codes/08 Crazy JS interview on Closures .js
@@ -55,3 +55,19 @@ function outest() {
 }
 var close = outest()("Hello from Akshad");
 close();
+
+//Conflicting name of global variable let in js
+function outest() {
+    function outer(b) {
+        var c = 20;
+        function inner() {
+            console.log(a, b, c);
+        }
+        let a = 10;
+        return inner;
+    }
+    return outer;
+}
+let a = 100;//but it will still print the value of a present in the function scope.
+var close = outest()("Hello from Akshad");
+close();
