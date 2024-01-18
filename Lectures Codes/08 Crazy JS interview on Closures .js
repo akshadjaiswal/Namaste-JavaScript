7906a83126b7cee0f35b97f5de70017c564b8eb9
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-18 19:46:23 +0530

    var to let

--- Lectures Codes/08 Crazy JS interview on Closures .js
+++ Lectures Codes/08 Crazy JS interview on Closures .js
@@ -14,7 +14,18 @@ function outer() {
     function inner() {
         console.log(a);
     }
-    var a = 10;//Even if we more the position of variable declartion it is still closure
+    var a = 10;//Even if we morve the position of variable declartion it is still closure
+    return inner;
+}
+outer()();
+
+//Changing var to let
+function outer() {
+    // var a=10;
+    function inner() {
+        console.log(a);
+    }
+    let a = 10;//Even if we change the var to let it is still closure
     return inner;
 }
 outer()();
