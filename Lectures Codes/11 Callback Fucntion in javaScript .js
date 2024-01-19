13a301e06ba301a499d026a7b4426e8408b52148
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-19 21:45:26 +0530

    Event listeners with code

--- Lectures Codes/11 Callback Fucntion in javaScript .js
+++ Lectures Codes/11 Callback Fucntion in javaScript .js
@@ -11,6 +11,17 @@ x(function y() {
 });
 
 //Event handler and listeners in javascript
-document.getElementById("clickMe").addEventListener("click", function xyz() {
-    console.log("button clicked");
-})
+document.getElementById("clickMe")
+    .addEventListener("click", function xyz() {
+        console.log("button clicked");
+    })
+
+//Event handler and listeners in javascript using closures finding the count of click.
+function attachEventListener() {
+    let count = 0;
+    document.getElementById("clickMe")
+        .addEventListener("click", function xyz() {
+            console.log("Button clicked", ++count);
+        })
+}
+attachEventListener();
