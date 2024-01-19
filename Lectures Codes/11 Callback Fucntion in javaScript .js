3c918bfdf57556d1fca9cc74aac3424dee31d79b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-19 21:00:29 +0530

    Callback function and settimeout

---
+++ Lectures Codes/11 Callback Fucntion in javaScript .js
@@ -0,0 +1,11 @@
+//What is callback function in the javascript
+setTimeout(function () {
+    console.log("timer")
+}, 5000);
+function x(y) {
+    console.log("x")
+    y()
+}
+x(function y() {
+    console.log("y")
+});
