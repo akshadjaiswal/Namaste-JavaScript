04dcdce643737c25d06e9d749412e2ffe5de15db
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-19 19:57:01 +0530

    Function statement and expression

---
+++ Lectures Codes/10 First Class Functions .js
@@ -0,0 +1,14 @@
+a();//It will call the function a
+// b();//It will throw an error undefined
+
+//Function Expression
+function a() {
+    console.log("a called");
+}
+//a()//you can call it from here 
+
+//Function Declaration
+var b = function () {
+    console.log("b called");
+}
+b()//you can only call it after the function
