b6d34107015557e2bce868fbfe4cb8d54be55eab
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-21 20:22:15 +0530

    Another example of event loop

--- Lectures Codes/12 Asynchronous Javascript and Event loops .js
+++ Lectures Codes/12 Asynchronous Javascript and Event loops .js
@@ -5,3 +5,11 @@ setTimeout(function cb() {
     console.log("Callback")
 }, 5000);
 console.log("End")
+
+//Example
+console.log("Start")
+document.getElementById("button")
+    .addEventListener("click", function cb() {//addeventlistener is the another power given by rhe browser to the engine
+        console.log("CallBack");
+    });
+console.log("End");
