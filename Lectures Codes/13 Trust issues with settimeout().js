32373285428cab11a9bcca1a4c05b7237eece14c
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-22 19:15:13 +0530

    Code demonstration for settimeout delay

--- Lectures Codes/13 Trust issues with settimeout().js
+++ Lectures Codes/13 Trust issues with settimeout().js
@@ -5,3 +5,14 @@ setTimeout(function a() {
     console.log("Callback")
 }, 5000);
 console.log("End")
+
+//Code demonstration for settimeout delay
+//It will bloc your main thread for the ten seconds so even settimeout setted for 5 second but it will be printed after the 10sec
+let startDate = new Date().getTime();
+let endDate = startDate;
+while (endDate < startDate + 10000) {
+    endDate = new Date().getTime();
+    // console.log(endDate)
+}
+
+console.log("while Expires")
