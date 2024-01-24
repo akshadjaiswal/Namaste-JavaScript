834d4d2040d2bfe5f29ec7e7883f7a26f6c93d5f
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-24 23:20:26 +0530

    normal method for solving question of area

--- Lectures Codes/14 Functional Programming .js
+++ Lectures Codes/14 Functional Programming .js
@@ -1,7 +1,20 @@
 //Basic example of higher order function
-function x(){
+function x() {
     console.log("Akshad");
 }
- function y(x){
-    
- }
+function y(x) {
+
+}
+
+//Normally used method to calculate area and circumferenece
+const radius = [1, 2, 3, 4, 5];
+
+const calculateArea = function (radius) {
+    const output = [];
+    for (let i = 0; i < radius.length; i++) {
+        output.push(Math.PI * radius[i] * radius[i]);
+    }
+    return output;
+}
+
+console.log(calculateArea(radius));
