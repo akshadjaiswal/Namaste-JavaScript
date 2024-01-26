2033d69cccb9cf383ad42f9ac0c77cae033be8c7
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-26 14:11:05 +0530

    triple the array

--- Lectures Codes/15 map, filter and reduce .js
+++ Lectures Codes/15 map, filter and reduce .js
@@ -4,5 +4,13 @@ const arr = [5, 1, 8, 7, 4];
 function double(x) {
     return x * 2;
 }
+
+//Triple  the array
+function triple(x) {
+    return x * 3
+}
+
 const output = arr.map(double);
+const output2 = arr.map(triple);
 console.log(output);
+console.log(output2);
