dc283c7a1ca0ab155aba58fdb6e3ad79665a0812
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-27 23:38:47 +0530

    callback hell

--- Lectures Codes/16 callback Hell .js
+++ Lectures Codes/16 callback Hell .js
@@ -6,3 +6,23 @@ setTimeout(function () {
 }, 5000);
 
 console.log("Jaiswal")
+
+//Here one call back in to another call back and another callback in another callback and soo on will create the large callback hell
+const cart = ["pants", "shoes", "watch"]
+
+api.createOrder(cart, function () {
+
+    api.makePayment(function () {
+
+        api.showSummary(
+            function () {
+                api.updateWallet()
+            }
+
+        )
+    })
+})
+
+
+
+
