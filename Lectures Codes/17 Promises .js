a64fed267abe82508a5bc9a7d09622074f608186
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-01-29 14:27:34 +0530

    promises function

---
+++ Lectures Codes/17 Promises .js
@@ -0,0 +1,18 @@
+//Promises
+const cart = ["Shoes", "Watches", "Flags"]
+
+// Below two functions are asynchronous and dependent on each other
+const orderId = createOrder(cart);
+procedToPayment(orderId);
+
+// with Callback (Before Promise)
+// Below here, it is the responsibility of createOrder function to first create the order then call the callback function
+createOrder(cart, function () {
+    procedToPayment(orderId)
+});
+// Above there is the issue of `Inversion of Control
+
+const promise = createOrder(cart);//// this promiseR has access to `then`
+// {data: undefined}
+// Initially it will be undefined so below code won't trigger
+// After some time, when execution has finished and promiseRef has the data then automatically the below line will get triggered.
