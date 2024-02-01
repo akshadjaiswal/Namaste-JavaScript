13d276003da4165abc77e060b0492ec5dedb7970
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-02-01 23:01:57 +0530

    async function with promise

--- Lectures Codes/20 async await  .js
+++ Lectures Codes/20 async await  .js
@@ -1,9 +1,18 @@
 // 💡 async function always returns a promise, even if I return a simple string from below function, async keyword will wrap it under Promise and then return.
 async function getData() {
-    return "Namaste JavaScript";
-  }
-  const dataPromise = getData();
-  console.log(dataPromise); // Promise {<fulfilled>: 'Namaste JavaScript'}
-  
-  //❓How to extract data from above promise? One way is using promise .then
-  dataPromise.then(res => console.log(res)); // Namaste JavaScript
+  return "Namaste JavaScript";
+}
+const dataPromise = getData();
+console.log(dataPromise); // Promise {<fulfilled>: 'Namaste JavaScript'}
+
+//❓How to extract data from above promise? One way is using promise .then
+dataPromise.then(res => console.log(res)); // Namaste JavaScript
+
+//async function
+async function getData2() {
+  return promise;//if we return the promise so it will never wrap in another promise
+}
+const data2 = getData2()
+
+data2.then(res => console.log(res))
+promise.then(res => console.log(res))//Both are same
