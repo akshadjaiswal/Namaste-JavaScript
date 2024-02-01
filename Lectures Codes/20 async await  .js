4bd1d6efceeb08284e1724041b7040fa799aa5ec
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-02-01 23:51:32 +0530

    how js engine works for async's wait

--- Lectures Codes/20 async await  .js
+++ Lectures Codes/20 async await  .js
@@ -8,6 +8,10 @@ console.log(dataPromise); // Promise {<fulfilled>: 'Namaste JavaScript'}
 //❓How to extract data from above promise? One way is using promise .then
 dataPromise.then(res => console.log(res)); // Namaste JavaScript
 
+//Creating a promise
+const promise = new Promise((res, rej) => {
+  res("Promise Resolved")
+})
 //async function
 async function getData2() {
   return promise;//if we return the promise so it will never wrap in another promise
@@ -19,12 +23,18 @@ promise.then(res => console.log(res))//Both are same
 
 //Async and await is use to handle promises
 const p = new Promise((resolve, reject) => {
-  resolve("Second promise reolved")
+  setTimeout(() => {
+    resolve("Second promise reolved")
+  },2000)
 })
 //handling through async await
 //await is a keyword than can only be used in async function
 async function promisehandler() {
+
+  console.log("Hello Akshad")
+  //JS engine is waiting for promisento get resolve
   const val = await p;
+  console.log("Namaste Akshad")
   console.log(val);
 }
 promisehandler();
