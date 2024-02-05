5ab4101f3582b9aa62416d3c375adc046bfa0ae2
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-02-05 14:58:01 +0530

    Error handling in fetch
    Using try and catch

--- Lectures Codes/21 async await example using fetch.js
+++ Lectures Codes/21 async await example using fetch.js
@@ -1,12 +1,17 @@
 //Real world example of async await by fetch call  by user URL api of github
 //"https://api.github.com/users/akshadjaiswal"
 const API_URL = "https://api.github.com/users/akshadjaiswal"
+// const API_URL = "https://ivalid url"
 
 async function promiseHandler() {
-    const data = await fetch(API_URL);
-    const jsonValue = await data.json();
-    console.log(jsonValue)
-
+    try { //Error Handling
+        const data = await fetch(API_URL);
+        const jsonValue = await data.json();
+        console.log(jsonValue)
+    }
+    catch (err) {
+        console.log(err);
+    }
     //fetch(API_URL)=Response.json()=>jsonvalue
 }
 promiseHandler();
