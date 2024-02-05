0857f3d76f03f18705e9621b3d624c9e4d153980
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-02-05 14:50:31 +0530

    fetch call working

--- Lectures Codes/21 async await example using fetch.js
+++ Lectures Codes/21 async await example using fetch.js
@@ -1,2 +1,12 @@
 //Real world example of async await by fetch call  by user URL api of github
 //"https://api.github.com/users/akshadjaiswal"
+const API_URL = "https://api.github.com/users/akshadjaiswal"
+
+async function promiseHandler() {
+    const data = await fetch(API_URL);
+    const jsonValue = await data.json();
+    console.log(jsonValue)
+
+    //fetch(API_URL)=Response.json()=>jsonvalue
+}
+promiseHandler();
