9e1e45fd55d54e0b84a284054fed88cce6495ca0
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-03-06 23:31:14 +0530

    commit

--- Lectures Codes/23 this keyword in javascript.js
+++ Lectures Codes/23 this keyword in javascript.js
@@ -9,13 +9,10 @@ console.log(test.func());
 // Expected output: 42
 
 const person = {
- name: "Akshad",
- age: 22,
-greet: function(){
-
-        return `Hello ${this.name}, you are ${this.age} years old`
-
-    }
-}
+  name: "Akshad",
+  age: 22,
+  greet: function () {
+    return `Hello ${this.name}, you are ${this.age} years old`;
+  },
+};
 console.log(person.greet());
-
