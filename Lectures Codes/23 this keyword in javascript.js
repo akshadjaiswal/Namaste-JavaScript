a3e010d6d5beade79ef21bf6562831740c2e2a3b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-04-29 23:15:26 +0530

    topic covered in episode 23

--- Lectures Codes/23 this keyword in javascript.js
+++ Lectures Codes/23 this keyword in javascript.js
@@ -16,3 +16,21 @@ const person = {
   },
 };
 console.log(person.greet());
+
+//this in global space
+
+//this in insdie function
+
+//this in stric t mode(this sbstitution)
+
+//this value depends on how this is called(window)
+
+//this inside a object method
+
+//call apply bind method (sharing method)
+
+//this insdie arrow function
+
+//this inside nested arrow function
+
+//this inside DOM
