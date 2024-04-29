7785341e003934989b6fe6730295ae39c38855ca
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-04-29 23:42:11 +0530

    This keyword

--- Lectures Codes/23 this keyword in javascript.js
+++ Lectures Codes/23 this keyword in javascript.js
@@ -1,31 +1,46 @@
-const test = {
-  prop: Akshad,
-  func: function () {
-    return this.prop;
-  },
-};
+// "use strict"
+// const test = {
+//   prop: Akshad,
+//   func: function () {
+//     return this.prop;
+//   },
+// };
 
-console.log(test.func());
-// Expected output: 42
+// console.log(test.func());
+// // Expected output: 42
 
-const person = {
-  name: "Akshad",
-  age: 22,
-  greet: function () {
-    return `Hello ${this.name}, you are ${this.age} years old`;
-  },
-};
-console.log(person.greet());
+// const person = {
+//   name: "Akshad",
+//   age: 22,
+//   greet: function () {
+//     return `Hello ${this.name}, you are ${this.age} years old`;
+//   },
+// };
+// console.log(person.greet());
 
 //this in global space
+console.log(this) //globalObject (window)
 
 //this in insdie function
-
+function x(){
+  //value depends on strict and non strrict mode
+  console.log(this)
+}
 //this in stric t mode(this sbstitution)
 
 //this value depends on how this is called(window)
+x();
+window.x();
 
 //this inside a object method
+ const obj={
+  a: "Akshad",
+  x: function (){
+    console.log(this);
+    console.log(this.a);
+  }
+ }
+ obj.x();
 
 //call apply bind method (sharing method)
 
