060e666ec611c820b03d8a17bb5b6f905accbee2
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-05-05 00:11:37 +0530

    this keyword
    this in nested arrow function and DOM

--- Lectures Codes/23 this keyword in javascript.js
+++ Lectures Codes/23 this keyword in javascript.js
@@ -19,40 +19,55 @@
 // console.log(person.greet());
 
 //this in global space
-console.log(this) //globalObject (window)
+console.log(this); //globalObject (window)
 
-//this in insdie function
-function x(){
+//this in inside a function
+function x() {
   //value depends on strict and non strrict mode
-  console.log(this)
+  console.log(this);
 }
-//this in stric t mode(this sbstitution)
+//this in strict mode(this sbstitution)
 
 //this value depends on how this is called(window)
 x();
 window.x();
 
 //this inside a object method
- const student={
+const student = {
   name: "Akshad",
-  printName: function (){
-    console.log/(this);
+  printName: function () {
+    console.log / this;
     console.log(this.name);
-  }
- }
- student.printName();
- 
- const student2={
-  name:"Aaradhana"
- }
- 
- //call apply bind method (sharing method)
- student.printName.call(student2);
+  },
+};
+student.printName();
+
+const student2 = {
+  name: "Aaradhana",
+};
+
+//call apply bind method (sharing method)
+student.printName.call(student2);
 //apply
 //bind
 
 //this insdie arrow function
-
+const obj = {
+  a: "akshad",
+  x: () => {
+    console.log(this);
+  },
+};
+obj.x();
 //this inside nested arrow function
-
-//this inside DOM
+const obj2 = {
+  a: "akshad",
+  x: function () {
+    const y = () => {
+      console.log(this);
+    };
+    y();
+  },
+};
+obj2.x();
+//this inside DOM =====> reference to HTML elements
