533b2db6df9342d4a5b3cf117ac26e8407668961
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-08-16 22:41:25 +0530

    Update 08 Set-Timeout + Closures interview questions.js

--- Lectures Codes/08 Set-Timeout + Closures interview questions.js
+++ Lectures Codes/08 Set-Timeout + Closures interview questions.js
@@ -33,3 +33,19 @@ function x() {
     console.log("Hello from Akshad ")
 }
 x();
+
+
+//Print the number 1 to 25 after each seconds using setTimeout by using var
+function x() {
+    //let has block scope and it creates the new copies i after every loop
+    for (var i = 1; i <= 25; i++) {
+        function close(x) {
+            setTimeout(function () {
+                console.log(x);
+            }, x * 5000);
+        }
+        close(i);//every time we call this function again it will provide the new value of i and now it will work with var
+    }
+    console.log("Hello from Akshad ")
+}
+x();
